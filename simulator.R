e7d09fc7b20c1f7fb45d69c107a47e2d1e712ed7
David Selby <david@selbys.org.uk>
2024-10-10 00:04:39 +0200

    Version 2 done

--- simulator.R
+++ simulator.R
@@ -17,7 +17,6 @@ Patient <- R6Class(
     time_since_treatment_started = NULL,
     time_since_last_flare = NULL,
     treatment_effectiveness = NULL,
-    days_until_effective = NULL,
     flare_status = NULL,
     days_until_flare_ends = NULL,
     
@@ -31,27 +30,26 @@ Patient <- R6Class(
       self$phenotype <- runif(1) # Simulate phenotype as a random value
       self$treatment_status <- "Off" # Initialize treatment status as "Off"
       self$latent_pain_status <- sample(c("High", "Medium", "Low"), 1,
-                                        prob = c(.4, .4, .2)) # Simulate latent pain status as a random value
+                                        prob = c(.1, .4, .5)) # Simulate latent pain status as a random value
       self$time_since_treatment_started <- Inf # Initialize time since treatment started as Inf
       self$time_since_last_flare <- sample(0:365, 1) # Simulate time since last flare as a random value between 0 and 365
       self$treatment_effectiveness <- NA # But no treatment = Ineffective treatment
-      self$days_until_effective <- NA
       self$flare_status <- "Off"
       self$days_until_flare_ends <- NA
     },
     
     simulate_treatment_effectiveness = function() {
-      sample(c("Effective", "Ineffective"), 1)
+      sample(c("Effective", "Ineffective"), 1, prob = c(.8, .2))
     },
     
     simulate_day = function() {
       self$day <- self$day + 1
 
       # Update latent pain status based on a simple hidden Markov model
-      transition_matrix <- matrix(c(0.8, 0.15, 0.05,
-                                    0.1, 0.8, 0.1,
-                                    0.05, 0.35, 0.5), nrow = 3, byrow = TRUE)
-      states <- c("High", "Medium", "Low")
+      transition_matrix <- matrix(c(0.9, 0.1, 0.00,
+                                    0.1, 0.85, 0.05,
+                                    0.05, 0.25, 0.7), nrow = 3, byrow = TRUE)
+      states <- c("Low", "Medium", "High")
       current_state <- match(self$latent_pain_status, states)
       self$latent_pain_status <- sample(states, 1, prob = transition_matrix[current_state, ])
       
@@ -61,49 +59,50 @@ Patient <- R6Class(
           self$treatment_status <- "On"
           self$time_since_treatment_started <- 0
           self$treatment_effectiveness <- self$simulate_treatment_effectiveness()
-          self$days_until_effective <- sample(1:7, 1) # Random number of days between 1 and 7
         }
       }
       
       # Update time since treatment started
       if (self$treatment_status == "On") {
         self$time_since_treatment_started <- self$time_since_treatment_started + 1
-        if (self$treatment_effectiveness == "Effective" && self$time_since_treatment_started >= self$days_until_effective) {
+        if (self$treatment_effectiveness == "Effective" && self$time_since_treatment_started <= 7) {
           # Reduce latent pain status by 1 or 2 states
-          new_state <- max(1, current_state - sample(1:2, 1))
+          new_state <- max(1, current_state - sample(1:2, 1, prob = c(.2, .8)))
           self$latent_pain_status <- states[new_state]
           #self$treatment_status <- "Off" # End treatment?
         }
       }
       
       # Determine if a flare occurs
-      if (self$time_since_last_flare > 21 && runif(1) < 0.01) { # Unlikely to occur
+      if (self$time_since_last_flare > 21 && self$latent_pain_status != "High" && runif(1) < 0.01) { # Unlikely to occur
         self$flare_status <- "On"
         self$time_since_last_flare <- 0
-        self$days_until_flare_ends <- sample(5:14, 1) # Random number of days between 5 and 14
-        # Increase latent pain status by 1 or 2 states
-        new_state <- min(3, current_state + sample(1:2, 1))
-        self$latent_pain_status <- states[new_state]
+        self$days_until_flare_ends <- 4 # assume flares are a spike
       }
       
       # Update time since last flare
       if (self$flare_status == "On") {
-        self$time_since_last_flare <- self$time_since_last_flare + 1
-        if (self$time_since_last_flare >= self$days_until_flare_ends) {
+        # Increase latent pain status by 1 or 2 states
+        new_state <- min(3, current_state + sample(1:2, 1))
+        self$latent_pain_status <- states[new_state]
+        self$days_until_flare_ends <- self$days_until_flare_ends - 1
+        if (self$days_until_flare_ends <= 0) {
           self$flare_status <- "Off" # End flare
           # Reduce latent pain status by 1 or 2 states
-          new_state <- max(1, current_state - sample(1:2, 1))
-          self$latent_pain_status <- states[new_state]
+          # new_state <- max(1, current_state - sample(1:2, 1))
+          # self$latent_pain_status <- states[new_state]
         }
       } else {
         self$time_since_last_flare <- self$time_since_last_flare + 1
       }
       
-      # Generate noisy ordinal pain report
-      pain_report <- switch(self$latent_pain_status,
-                            "High" = sample(8:10, 1),
-                            "Medium" = sample(4:7, 1),
-                            "Low" = sample(0:3, 1))
+      # Pain levels associated with each state
+      pain_levels <- list(
+          Low = 0:3,
+          Medium = 4:7,
+          High = 8:10
+      )
+      pain_report <- sample(pain_levels[[self$latent_pain_status]], 1)
       
       # Record the day's data
       list(
@@ -131,29 +130,32 @@ Patient <- R6Class(
   )
 )
 
+set.seed(123)
 # Simulate 10 patients each for 6 months
-patients_data <- purrr::map_dfr(1:12, function(id) {
+patients_data <- purrr::map_dfr(1:10, function(id) {
     patient <- Patient$new()
     traj <- patient$simulate_6_months()
     traj$id <- id
     traj
 })
 
-ggplot(patients_data %>%
-       mutate(flare_status = factor(flare_status, c('On', 'Off')))) +
+ggplot(patients_data) +
   aes(day, ordinal_pain_report) +
-  geom_vline(aes(xintercept = day), colour = 'steelblue', alpha = .25,
-             data = filter(patients_data, start_of_treatment)) +
-#   geom_line(alpha = .3) +
-  geom_point(aes(fill = flare_status), shape = 21, colour = 'white') +
-  geom_rug(aes(y = NULL, colour = latent_status)) +
+  geom_vline(aes(xintercept = day, linetype = treatment_effectiveness), colour = 'seagreen', alpha = .25,
+             data = filter(patients_data, start_of_treatment), linewidth = 1) +
+  geom_vline(aes(xintercept = day), colour = 'red', alpha = .25,
+             data = filter(patients_data, flare_status == 'On'), linewidth = 1) +
+  geom_line(alpha = .1) +
+  geom_point(size = 1) +
+  geom_rug(aes(colour = latent_status)) +
   scale_y_continuous(breaks = seq(0, 10, by = 2), minor_breaks = NULL) +
-  facet_wrap(~ id, nrow = 4) +
+  facet_wrap(~ id, ncol = 2) +
   guides(fill = guide_legend(title = "Flare"), 
          linetype = guide_legend(title = "Treatment"), 
          colour = guide_legend(title = "Disease")) +
-  scale_color_manual(values = c('#B3E2B3', '#DDDD55', '#FFB6C1')) +
+  scale_color_manual(values = rev(c('#B3E2B3', '#DDDD55', '#FFB6C1'))) +
   scale_fill_manual(values = c('tomato', 'grey40')) +
-  theme_minimal() +
+  theme_classic() +
   ylab('Reported pain')
 
+ggsave('trajectories.png', width = 16, height = 8)
